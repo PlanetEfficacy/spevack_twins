5097455685a45e2075486bc69b0b878c5a690f37
PlanetEfficacy <jspevack@gmail.com>
2017-02-09 21:43:48 -0700

    Add basic index page

--- app/assets/javascripts/components/flat_button.es6.jsx
+++ app/assets/javascripts/components/flat_button.es6.jsx
@@ -1,7 +1,8 @@
 class FlatButton extends React.Component {
   render () {
     return (
-      <a className="waves-effect waves-light btn-large">
+      <a onClick={this.props.handleClick}
+         className="waves-effect waves-light btn-large">
         <i className="material-icons left">{ this.props.icon }</i>
         { this.props.text }
       </a>
--- app/assets/javascripts/components/main_photo.es6.jsx
+++ app/assets/javascripts/components/main_photo.es6.jsx
@@ -1,15 +1,19 @@
 class MainPhoto extends React.Component {
+  componentDidMount() {
+    $('.materialboxed').materialbox();
+  }
+
   render () {
     return (
       <div className="card">
         <div className="card-image">
           <img className="materialboxed"
-               data-caption={ this.props.caption }
                src={ this.props.image }/>
                <span className="card-title">{ this.props.title }</span>
         </div>
         <div className="card-content">
           <p>{ this.props.caption }</p>
+          <p>{ this.props.date }</p>
         </div>
       </div>
     )
