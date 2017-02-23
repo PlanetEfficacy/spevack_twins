e01f391d2efba9fb0df570fc44b2edb5fc80bc40
PlanetEfficacy <jspevack@gmail.com>
2017-02-23 11:50:11 -0700

    Add crud comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,9 +3,36 @@ class Api::V1::CommentsController < ApplicationController
     render json: photo.comments
   end
 
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment
+    end
+  end
+
+  def update
+    comment.update(comment_params)
+    if comment.save
+      render json: comment
+    end
+  end
+
+  def destroy
+    comment.delete
+    head :no_content
+  end
+
   private
 
   def photo
     Photo.find(params[:photo_id])
+  end
+
+  def comment
+    Comment.find(params[:id])
+  end
+
+  def comment_params
+    { photo: photo, user: current_user, body: params[:comment][:body] }
   end
 end
